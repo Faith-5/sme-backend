7cb4ca00e02c310a9ca6a239b51c491b6828fe3f
Bamidele Faith <bamidelefaith100@gmail.com>
2026-03-06 13:35:11 +0100

    Updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,6 @@ from app.db.database import engine
 from app.db import models
 from app.routes import auth_routes
 
-# DROP old tables (dangerous if you have data!)
-models.Base.metadata.drop_all(bind=engine)
-
 # CREATE new tables from your current models
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,3 +16,7 @@ app.include_router(auth_routes.router)
 @app.get('/')
 def read_root():
     return {"message": "SME API is running successfully!"}
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
